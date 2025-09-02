16f99e964bf8fee6c44d232a40e7ac818ca47528
Shariq Alam <sharo4192@gmail.com>
2025-09-02 15:15:27 +0500

    React Started

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+    <h1>Hello World | Shariq Alam</h1>
+    <p>I am Shariq Alam A Web Developer Using React Vite :</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus incidunt quis odit facilis! Illo voluptatibus qui repudiandae molestiae voluptatum commodi tenetur delectus ut id autem, inventore unde libero ipsa labore.
+    Eligendi quasi, ab, laborum aut quae tempora, nulla fuga tempore similique corporis ipsa iusto eius dignissimos. Beatae, id? At nam itaque ratione. Harum autem ipsa hic esse </p>
+  </>
+  )
+}
+
+export default App
