928d96c658a2d81d2b6f52ebfcc245c92c087575
Shariq Alam <sharo4192@gmail.com>
2025-09-09 10:06:24 +0500

    Custom React Comeplete

--- React_all/CustomReact/customReact.js
+++ React_all/CustomReact/customReact.js
@@ -9,6 +9,15 @@ function customRender(reactElement, mainContainer){
 
 */
 
+const domElement = document.createElement(reactElement.type)
+domElement.innerHTML = reactElement.children
+for (const prop in reactElement.props) {
+    if(prop === "children") continue;
+    domElement.setAttribute(prop, reactElement.props[prop])
+}
+mainContainer.appendChild(domElement)
+
+
 }
 
 const reactElement = {
