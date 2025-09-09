caa73a0bb7e9ecd7cce7d5035385d73dde1b9822
Shariq Alam <sharo4192@gmail.com>
2025-09-09 11:51:34 +0500

    A Little Counter Project With Hooks

---
+++ React_all/02counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+  
+  let [counter , setCounter] = useState(0)
+  
+  // let counter = 15
+  const addValue = () => {
+    
+    // counter = counter + 1    
+    if(counter < 20){
+    setCounter(counter + 1)
+   }
+ }
+
+  const removeValue = ()=>{
+    if(counter > 0){
+    setCounter(counter - 1)
+    }
+  }
+
+  return (
+    <>
+    <h1>Chai Aur React</h1>
+    <h2>counter value : {counter}</h2>
+    <p>Number Only Goes From 0 To 20</p>
+    <button onClick={addValue} >Add Value</button>
+    <br />
+    <br />
+    <button onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
