86404854a649ef6dda8f58f9f8e809076ec3d8d6
Shariq Alam <sharo4192@gmail.com>
2025-09-10 14:54:30 +0500

    A little Project using tailwind css and props

---
+++ React_all/03tailwindprops/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './components/card'
+
+function App() {
+
+useState
+
+  return (
+    <>
+    <h1 className='bg-green-400 text-black p-4 rounded'>Tailwind Test</h1>
+    <Card Username="Shariqalam" btnText="Click Me" />
+    <Card Username="Sharo" btnText="Visit Me"/>
+    </>
+  ) 
+}
+
+export default App
