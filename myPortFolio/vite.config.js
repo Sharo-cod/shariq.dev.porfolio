5c9cd2bb53326349c2c3d4b940480586c3d2c576
Shariq Alam <sharo4192@gmail.com>
2025-10-15 15:32:01 +0500

    vite.config

--- myPortFolio/vite.config.js
+++ myPortFolio/vite.config.js
@@ -1,21 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  assetsInclude: ['**/*.JPG', '**/*.jpg'],
-
-  // 👇 Set base path (replace with your repo name)
-  base: '/myPortFolio/',
-
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-})
+  assetsInclude: ['**/*.JPG', '**/*.jpg'], // allows JPG imports
+});
